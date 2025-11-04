16e3b00a8bc779e7945d0bcd5c6971c0641afb09
hemreozalp <h.emreozalpp@gmail.com>
2025-11-04 17:36:01 +0300

    initial setup

---
+++ src/test/java/com/hemreozalp/spring_mvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemreozalp.spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
